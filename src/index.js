9a17b81a139ec9130d7eb126090c3d2408498f70
Igor Manelov <igor-manelov@mail.ru>
2020-04-05 19:46:39 +0300

    feat: complete EZ task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function reverse (n) {
-  
+   return Math.abs(n).toString().split('').reverse().join('');
 }
+//input = -468 => |-468| => '468' => [4, 6, 8] => [8, 6, 4] => [864] = output
